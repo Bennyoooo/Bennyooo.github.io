986429237ff0792fecd615bf13138f1a4fcd5a14
Bennyoooo <bennyjiangxinhao@gmail.com>
2021-01-07 02:56:08 +0800

    Updates

---
+++ precache-manifest.b5e33f544a2979221889bc41209e4caf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "563c09ab016b202aec62bf46cfff646e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4911410f508b245a7ed8",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4911410f508b245a7ed8",
+    "url": "/static/js/main.37314940.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "83e2ac555cf8258175a32cad868c4322",
+    "url": "/static/media/bennyjiang.83e2ac55.png"
+  },
+  {
+    "revision": "b143a70833c3650a2194b02045b979b7",
+    "url": "/static/media/resume.b143a708.pdf"
+  }
+]);
